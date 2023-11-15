a96fc22e1853e994ae03186d2e57ebc921facf4b
SARAAG <saraag0036@gmail.com>
2023-11-15 23:23:17 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Oracle on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
