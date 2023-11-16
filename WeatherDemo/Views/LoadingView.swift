dcb05d0c2181b3c34f592e93e0976d0417277b08
s3r71g <saraag0036@gmail.com>
2023-11-16 18:49:56 +0530

    Design+Code version

---
+++ WeatherDemo/Views/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  WeatherDemo
+//
+//  Created by Oracle on 16/11/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
